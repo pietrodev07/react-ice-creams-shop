d0a05a0ca39b49ef5d4a614aeb5b6512cf221dbe
pietrodev07 <pietro.dev.07@gmail.com>
2023-06-05 16:56:47 +0200

    feat: add error component

---
+++ src/components/Utils/Error/Error.jsx
@@ -0,0 +1,23 @@
+const Error = () => {
+
+  const errorStyle = {
+
+    display: 'flex',
+
+    justifyContent: 'center',
+
+    alignItems: 'center',
+
+    gridColumn: 'span 3',
+
+  }
+
+  return (
+
+    <h2 style={errorStyle}>Error...</h2>
+
+  )
+
+}
+
+export default Error
