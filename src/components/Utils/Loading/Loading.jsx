3e0c1d3885f4972e9d95e4f22ccf6eaff09ed87b
pietrodev07 <pietro.dev.07@gmail.com>
2023-06-05 16:57:54 +0200

    feat: add loading component

---
+++ src/components/Utils/Loading/Loading.jsx
@@ -0,0 +1,23 @@
+const Loading = () => {
+
+  const loadingStyle = {
+
+    display: 'flex',
+
+    justifyContent: 'center',
+
+    alignItems: 'center',
+
+    gridColumn: 'span 3',
+
+  }
+
+  return (
+
+    <h2 style={loadingStyle}>Loading...</h2>
+
+  )
+
+}
+
+export default Loading
