43fead8f4bd7c6def1fd334420d448f764d18dba
pietrodev07 <pietro.dev.07@gmail.com>
2023-06-05 17:29:55 +0200

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header'
 import IceCreams from './components/IceCreams/IceCreams'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Header />
 
       <IceCreams />
+
+      <Footer />
 
     </div>
 
